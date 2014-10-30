ed60bc2bc4019697d34017c479faebea92ba12c2
Britny Herzog <britny@bitium.com>
2014-10-29 17:50:22 -0700

    Added functional edit button, Displays error on initial amount input

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,4 +4,15 @@ class ListingsController < ApplicationController
   	@listVariable = Listing.all #fetches stuff
   end
 
+  def edit
+  	@listVariable = Listing.find(params[:id])
+  end
+
+  def update
+  	listVariable = Listing.find(params[:id])
+    listVariable.amount = params[:amountRequested]
+    listVariable.save  #TO DO: make sure if bad value don't redirect, gives an error, lets people change it. 
+  	redirect_to listings_path
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,10 +8,15 @@ class WelcomeController < ApplicationController
    l = Listing.new #creates l as a new instance of Listing class
    l.mutant = params[:mutant] #sets equal to mutant option in form
    l.amount = params[:amount] #sets equal to text amount in form
+
+   l.valid? # checks if l is a valid entry
+
    if l.save #saves as a listing
      flash[:message] = 'Created Listing!'
    else
-     flash[:message] = 'Sorry, there was an error.'
+     #flash[:message] = 'Sorry, there was an error.'
+     #raise l.errors.inspect
+     flash[:message] = l.errors[:amount].first #should print out specific nature of error. Where are the [""] coming from?
    end
    redirect_to root_path #refreshes page back to first form once submitted
   end
