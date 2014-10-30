60abade43a8bbc121f64f9823546f2101ac7a245
Britny Herzog <britny@bitium.com>
2014-10-29 17:53:25 -0700

    Added some comments

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,18 +1,18 @@
 class ListingsController < ApplicationController
   
   def index
-  	@listVariable = Listing.all #fetches stuff
+  	@listVariable = Listing.all #fetches all the listings
   end
 
   def edit
-  	@listVariable = Listing.find(params[:id])
+  	@listVariable = Listing.find(params[:id]) #fetches the particular listing
   end
 
   def update
-  	listVariable = Listing.find(params[:id])
-    listVariable.amount = params[:amountRequested]
+  	listVariable = Listing.find(params[:id]) #fetches a particular listing.
+    listVariable.amount = params[:amountRequested] #updates the amount requested to the amount in the form edit.html.erb
     listVariable.save  #TO DO: make sure if bad value don't redirect, gives an error, lets people change it. 
-  	redirect_to listings_path
+  	redirect_to listings_path #redirects so a person can't submit twice. 
   end
 
 end
